b288e4a8785abbd25ff04a7ba759cd3a92a7a907
Emelifonwu William <emelifonwuw@gmail.com>
2024-02-10 11:44:46 +0100

    changes made to database connection

--- conn.js
+++ conn.js
@@ -2,10 +2,14 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "student360",
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
 });
 
 const connectToDatabase = () => {
